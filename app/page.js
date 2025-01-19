b0a72e741f4d8aabd8cc04ce759c6c333a9e27fc
DaanFrankhuizen <daan.frankhuizen@student.hu.nl>
2025-01-19 18:29:51 +0100

    removed ESLint due to error

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
-// pages/index.js (of pages/home.js, afhankelijk van je voorkeur)
-import React from 'react';
+import Link from 'next/link';
 
-export default function HomePage() {
+const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-white text-center font-sans">
       <h1 className="text-5xl font-bold text-white">Daan Frankhuizen</h1>
       <p className="mt-4 text-2xl">Webdeveloper | Designer | Creative Problem Solver</p>
-      <a href="/about" className="mt-6 px-8 py-4 bg-blue-700 text-white font-bold rounded-lg hover:bg-blue-800 transition-all duration-300">
-        Learn More About Me
-      </a>
+      {/* <Link href="/about">
+        <a className="mt-6 px-8 py-4 bg-blue-700 text-white font-bold rounded-lg hover:bg-blue-800 transition-all duration-300">
+          Learn More About Me
+        </a>
+      </Link> */}
     </div>
   );
 };
+
+export default HomePage;
